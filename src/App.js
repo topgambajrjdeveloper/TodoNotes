6a9ca62cf0b50320bac7163895d0b0f487cf6e9f
julian rodriguez jimenez <topgambajrjdeveloper@gmail.com>
2020-08-07 09:05:03 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import { Header } from "./components/Layout/Header";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
---
+++ src/components/layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Header = () => {
+  const x = 1;
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          <img src="/images/papel.svg" alt="TodoIst" />
+        </div>
+        <div className="settings">
+          <ul>
+            <li>+</li>
+            <li>Pizza Slice!</li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
